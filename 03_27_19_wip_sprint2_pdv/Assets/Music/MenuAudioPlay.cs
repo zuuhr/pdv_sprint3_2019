a5ee973c8056b0a27cac5dcec6dc63f24faeb8ff
AruxG <antoniorobledinosanton@gmail.com>
2019-04-20 21:14:46 +0200

    Música de fondo implementada

---
+++ 03_27_19_wip_sprint2_pdv/Assets/Music/MenuAudioPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAudioPlay : MonoBehaviour
+{
+    public AudioSource a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("audioPlay", 27.28f);
+    }
+
+    // Update is called once per frame
+    void audioPlay()
+    {
+        a.Play();
+    }
+}
