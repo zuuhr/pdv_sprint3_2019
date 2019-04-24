90585c1217271c00762f8c0fbf9eca1217f931aa
Darcor\Zuuhr <zuuhr.contact@gmail.com>
2019-04-24 22:10:12 +0200

    limpiando

--- 03_27_19_wip_sprint2_pdv/Assets/Scripts/alien_movementNew.cs
+++ 03_27_19_wip_sprint2_pdv/Assets/Scripts/alien_movementNew.cs
@@ -10,20 +10,13 @@ public class alien_movementNew : MonoBehaviour
     public int speed;
     private int xRand;
     private int zRand;
-    public float shootingRespawnTime; //gap time between bullet firing
-    float timeLimit; //gap time in real time units 
-    public GameObject bullet;
     public GameObject impact;
 
     void Start()
     {
-        timeLimit = Time.time + shootingRespawnTime;
         xRand = (int)Random.Range(-10, 10);
         zRand = (int)Random.Range(-10, 10);
         movement = new Vector3(xRand, 0, zRand) * speed;
-
-        //transform.forward = Player_movement_2DNew.thisPosition - transform.position;
-        
     }
 
     void Update()
@@ -60,11 +53,7 @@ public class alien_movementNew : MonoBehaviour
         {
             SceneManager.LoadScene("End_13plus"); //Game Over scene
         }
-        /*if (collider.tag == "Wall") //if alien hits a defensive wall
-        {
-            Destroy(collider.gameObject);
-            Destroy(gameObject);
-        }*/
+        
     }
 
     
