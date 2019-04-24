3f1fb5b9e67ce192cc9faa6bf8d39bc5b6e78483
brenlli <brenllifsca5@gmail.com>
2019-04-24 20:15:46 +0200

    commit de impacto a medais

--- 03_27_19_wip_sprint2_pdv/Assets/Scripts/outerImpact.cs
+++ 03_27_19_wip_sprint2_pdv/Assets/Scripts/outerImpact.cs
@@ -12,6 +12,7 @@ public class outerImpact : MonoBehaviour
 
     void Update()
     {
+        transform.LookAt(new Vector3(0,0,0));
         if (Time.time > timeSpan) Destroy(gameObject);
     }
 }
